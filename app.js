7d09e5663c472c7b52bf6e58563bb10689afc5ae
RedBalloon24 <worldtraveler757@gmail.com>
2020-03-13 14:37:30 +0100

    create campgrounds

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
 var express = require("express")
 var app = express()
+var bodyParser = require("body-parser")
 
+app.use(bodyParser.urlencoded({extended: true}))
 app.set("view engine", "ejs")
+
+var campgrounds = [
+    {name: "Turtle Pond", image: "https://images.unsplash.com/photo-1471115853179-bb1d604434e0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=959&q=80"},
+    {name: "Walnut Hill", image: "https://images.unsplash.com/photo-1492648272180-61e45a8d98a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"},
+    {name: "Mountine Pine", image: "https://images.unsplash.com/photo-1487730116645-74489c95b41b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"}
+]
 
 app.get("/", (req, res) => {
     res.render("landing")
 })
 app.get("/campgrounds", (req, res) => {
-    var campgrounds = [
-        {name: "Turtle Pond", image: "https://images.unsplash.com/photo-1471115853179-bb1d604434e0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=959&q=80"},
-        {name: "Walnut Hill", image: "https://images.unsplash.com/photo-1492648272180-61e45a8d98a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"},
-        {name: "Mountine Pine", image: "https://images.unsplash.com/photo-1487730116645-74489c95b41b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"}
-    ]
-
     res.render("campgrounds", {campgrounds: campgrounds})
+})
+app.get("/campgrounds/new", (req, res) => {
+    res.render("new")
+})
+app.post("/campgrounds", (req, res) => {
+    var name = req.body.name
+    var image = req.body.name
+    var newCampground = {name: name, image: image}
+
+    campgrounds.push(newCampground)
+    
+    res.redirect("/campgrounds")
 })
 
 
