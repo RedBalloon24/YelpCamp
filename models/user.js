e56d7f8f391ed1669b245dff645afdcb086277ab
RedBalloon24 <worldtraveler757@gmail.com>
2020-04-08 14:01:43 +0200

    mvp 3

--- models/user.js
+++ models/user.js
@@ -1,12 +1,12 @@
 var mongoose = require("mongoose");
 var passportLocalMongoose = require("passport-local-mongoose");
 var URL_PATTERN = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
-
+var USERNAME_PATTERN = /^(?=.{5,20}$)([A-Za-z._](?:(?:[A-Za-z0-9._]|(?:\.(?!\.))){1,28}(?:[A-Za-z0-9._]))?)$/gi;
 
 var UserSchema = new mongoose.Schema({
     username: {
         type: String,
-        minlength: [3, "Username needs at least 3 characters"]
+        match: [USERNAME_PATTERN, "Must have minimum 5 characters. May include numbers and special characters '.' and '_'."]
     },
     password: {
         type: String,
